9e303df6aee1e0622b6de17233cadd321a19400d
nocturn9x <hackhab@gmail.com>
2020-02-01 06:40:03 +0000

    Minor style fix to README

--- example.py
+++ example.py
@@ -3,14 +3,13 @@ from AsyncAPY.base import AsyncAPY
 from AsyncAPY.filters import Filters
 from AsyncAPY.objects import Packet, Client
 
-srv = AsyncAPY(port=1500, logging_level=logging.DEBUG, addr="localhost")
+srv = AsyncAPY(port=1500, logging_level=logging.DEBUG, addr="localhost", proto='ziproto')
 
 
 @srv.handler_add("ping", filters=[Filters.Ip("127.0.0.1")])
 async def ping(client: Client, packet: Packet):
     await client.send(Packet({"status": "PONG!"}, None))
-#    await packet.stop_propagation()
-
+    await packet.stop_propagation()
 
 @srv.handler_add("ping", filters=[Filters.Ip("127.0.0.1")], priority=1)
 async def another_ping(client: Client, packet: Packet):
