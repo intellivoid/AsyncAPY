fba2de1e6799bd2aac09a0db5797b1d32164f0f5
nocturn9x <hackhab@gmail.com>
2020-02-09 21:36:05 +0000

    Improved tests

--- tests/tests_asyncapy.py
+++ tests/tests_asyncapy.py
@@ -6,10 +6,12 @@ import ziproto
 
 class TestAsyncAPY:
 
-   def test_headers(self):
+
+    def test_headers(self):
        client = defaultclient.Client("127.0.0.1", 1500)
+       enc = 'json'
        client.connect()
-       client.send({"test": 1})
+       client.send({"test": 1}, enccoding=enc)
        response = client.receive_all()
        content_length = int.from_bytes(response[0:client.header_size], client.byteorder)
        protocol_version = int.from_bytes(response[client.header_size:client.header_size + 1], "big")
@@ -17,5 +19,10 @@ class TestAsyncAPY:
        assert len(response[client.header_size:]) == content_length, content_length
        assert protocol_version in (11, 22), protocol_version
        assert content_encoding in (0, 1), content_encoding
+       if enc == 'json':
+           payload = json.loads(response[client.header_size + 3:]
+       else:
+           payload = ziproto.decode(response[client.header_size + 3:])
+       assert payload == {"test": 1}
        client.disconnect()
 
