97b4515406fa7588461a087634f94e2c2730eaf7
nocturn9x <hackhab@gmail.com>
2020-02-12 18:40:34 +0100

    Updated examples with groups examples

--- docs/examples.rst
+++ docs/examples.rst
@@ -8,3 +8,13 @@ Filters Examples
 Here is an example of how to use AsyncAPY's ``Filter`` objects
 
 .. literalinclude:: ../examples/filters.py
+
+Here is an example of how to use AsyncAPY's grouping features
+
+.. literalinclude:: ../examples/groups.py
+
+.. note::
+    It is possible to group handlers with filters, too, as long as they
+    share the same filters, not necessarily in the same order, and have
+    different priorities
+
---
+++ examples/groups.py
@@ -0,0 +1,20 @@
+from AsyncAPY.base import AsyncAPY
+
+server = AsyncAPY(addr='127.0.0.1', port=1500, encoding="json", logging_level=10, byteorder='big')
+
+
+@server.handler_add()
+async def echo_server(client, packet):
+    print(f"Hello world from {client}!")
+    print(f"Echoing back {packet}...")
+    await client.send(packet, close=False)
+    # packet.stop_propagation()  # Uncomment to prevent the packet from being forwarded to the next handler
+
+
+@server.handler_add(priority=1)    # Adding a priority is compulsory, or a RuntimeError exception will be raised
+async def echo_server(client, packet):
+    print(f"Hello world from {client} inside a group!")
+    print(f"Echoing back {packet}...")
+    await client.send(packet)
+
+server.start()
