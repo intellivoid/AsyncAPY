390f6bc628ca45d30528ab46faf6c308133ad1c1
nocturn9x <hackhab@gmail.com>
2020-03-25 06:51:29 +0000

    Bumped version number

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Intellivoid Technologies'
 author = 'Intellivoid Technologies'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.0'
+release = '0.3.3'
 
 # -- General configuration ---------------------------------------------------
 
