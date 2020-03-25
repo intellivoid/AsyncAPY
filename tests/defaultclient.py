e159f86ed97c6ff5fbc407400ffb4ee14a603735
nocturn9x <hackhab@gmail.com>
2020-03-25 06:51:10 +0000

    Bumped version number

--- tests/defaultclient.py
+++ tests/defaultclient.py
@@ -3,6 +3,7 @@ import ziproto
 import json
 import time
 import ssl
+import logging
 
 
 ADDR = '127.0.0.1'
@@ -18,7 +19,7 @@ class Client:
        :type addr: str
        :param port: The port of the server to connect to
        :type port: int
-       :param byteorder: The client's byteorder, it can either be ``"json"`` or ``"ziproto"``, default to ``"json"``
+       :param byteorder: The client's byteorder, it can either be ``"big"`` or ``"little"``, detault to ``"big"``
        :type byteorder: str
        :param header_size: The size of the ``Content-Length`` header, defaults to 4
        :type header_size: int
@@ -33,7 +34,7 @@ class Client:
             raise ValueError("address must be string!")
         if not isinstance(port, int):
             raise ValueError("port must be an integer!")
-        if not byteorder in ("big", "little"):
+        if byteorder not in ("big", "little"):
             raise ValueError("byteorder must either be big or little!")
         if not isinstance(header_size, int):
             raise ValueError("header_size must be an integer!")
@@ -43,6 +44,8 @@ class Client:
         self.header_size = header_size
         self.sock = None
         self.tls = tls
+        self._extra_data = None
+
 
     def connect(self):
         """Connects to ``self.addr:self.port``"""
@@ -54,17 +57,23 @@ class Client:
         self.sock.connect((self.addr, self.port))
 
     def disconnect(self):
-        """Disconnects from the server"""
+        """Closes ``self.sock``"""
 
         self.sock.close()
 
     def send(self, payload: dict or str, encoding: str = "json"):
-        """Encodes the passed payload and sends it across the socket"""
+        """Encodes the passed payload and sends it across the socket, please note that this method does NOT catch any exception if an error occurs
+
+           :param payload: The payload to send as a dictionary object or a valid JSON serializable string
+           :type payload: dict or str, optional
+           :param encoding: The encoding of the payload, note that this should be kept consistent across the session, as the encoding of responses cannot change during a session once defined (your packets will still be decoded properly, but you won't get the same encoding of your payloads)
+           :type encoding: str, optional
+        """
 
         if isinstance(payload, dict):
-            payload = json.dumps(payload).encode("latin-1")
+            payload = json.dumps(payload).encode()
         if encoding == "ziproto":
-            payload = ziproto.encode(json.loads(payload))
+            payload = ziproto.encode(payload)
         content_length = (len(payload) + 2).to_bytes(self.header_size, self.byteorder)
         content_encoding = (0).to_bytes(1, "big") if encoding == "json" else (1).to_bytes(1, "big")
         protocol_version = (22).to_bytes(1, "big")
@@ -72,7 +81,7 @@ class Client:
         packet = headers + payload
         self.sock.sendall(packet)
 
-    def rebuild_stream(self, size: int):
+    def _rebuild_stream(self, size: int):
         """Rebuilds a stream if too short"""
 
         data = b""
@@ -81,13 +90,13 @@ class Client:
             try:
                 data += self.sock.recv(size)
             except Exception as error:
-                print(f"An error occurred while reading from socket -> {error}")
+                logging.error(f"An error occurred while reading from socket -> {error}")
             if not len(data) >= size:
                 time.sleep(0.1)
                 times += 1
             if times == 600:
-                print("The 60 seconds timeout for reading the socket has expired, exiting...")
-                break
+                logging.debug("The 60 seconds timeout for reading the socket has expired, exiting...")
+                return None
 
         return data
 
@@ -96,25 +105,31 @@ class Client:
 
         data = b""
         times = 0
+        if self._extra_data:
+            data += self._extra_data
+            self._extra_data = None
         while len(data) < self.header_size:
             try:
                 data += self.sock.recv(1024)
-            except Exception as error:
-                print(f"An error occurred while reading from socket -> {error}")
+            except socket.error as error:
+                logging.error(f"An error occurred while reading from socket -> {error}")
+                return None
             if not len(data) >= self.header_size:
                 time.sleep(0.1)
                 times += 1
             else:
                 content_length = int.from_bytes(data[0:self.header_size], self.byteorder)
                 if len(data[self.header_size:]) < content_length:
-                    print(f"Stream is fragmented, attempting to rebuild")
-                    missing = self.rebuild_stream(content_length)
+                    logging.debug(f"Stream is fragmented, attempting to rebuild")
+                    missing = self._rebuild_stream(content_length)
                     if missing:
                         data += missing
                     else:
-                        print("Could not rebuild stream, bye")
-                        break
+                        logging.error("Could not rebuild stream")
+                        return None
             if times == 600:
-                print("The 60 seconds timeout for reading the socket has expired, exiting...")
+                logging.debug("The 60 seconds timeout for reading the socket has expired, exiting...")
                 break
-        return data
+        if len(data[self.header_size:content_length]) > content_length:
+            self._extra_data = data[content_length + 1:]
+        return data[self.header_size:content_length]
